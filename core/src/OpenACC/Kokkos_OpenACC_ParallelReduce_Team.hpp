90a246b0270a491ecdd618fc305c50099642517d
Damien L-G <dalg24@gmail.com>
2022-12-12 12:55:58 -0500

    Misc cleanup

--- core/src/OpenACC/Kokkos_OpenACC_ParallelReduce_Team.hpp
+++ core/src/OpenACC/Kokkos_OpenACC_ParallelReduce_Team.hpp
@@ -19,10 +19,9 @@
 
 #include <OpenACC/Kokkos_OpenACC_Team.hpp>
 #include <OpenACC/Kokkos_OpenACC_FunctorAdapter.hpp>
+#include <Kokkos_Parallel.hpp>
+#include <type_traits>
 
-//----------------------------------------------------------------------------
-//----------------------------------------------------------------------------
-// Hierarchical Parallelism -> Team level implementation
 namespace Kokkos::Experimental::Impl {
 
 // primary template: catch-all non-implemented custom reducers
@@ -38,64 +37,60 @@ struct OpenACCParallelReduceTeamHelper {
 
 }  // namespace Kokkos::Experimental::Impl
 
-template <class FunctorType, class ReducerType, class... Properties>
-class Kokkos::Impl::ParallelReduce<FunctorType,
-                                   Kokkos::TeamPolicy<Properties...>,
+template <class Functor, class ReducerType, class... Traits>
+class Kokkos::Impl::ParallelReduce<Functor, Kokkos::TeamPolicy<Traits...>,
                                    ReducerType, Kokkos::Experimental::OpenACC> {
- private:
-  using Policy =
-      TeamPolicyInternal<Kokkos::Experimental::OpenACC, Properties...>;
+  using Policy = TeamPolicyInternal<Kokkos::Experimental::OpenACC, Traits...>;
 
   using ReducerConditional =
-      Kokkos::Impl::if_c<std::is_same<InvalidType, ReducerType>::value,
-                         FunctorType, ReducerType>;
+      if_c<std::is_same_v<InvalidType, ReducerType>, Functor, ReducerType>;
   using ReducerTypeFwd = typename ReducerConditional::type;
   using Analysis =
       FunctorAnalysis<FunctorPatternInterface::REDUCE, Policy, ReducerTypeFwd>;
-  using value_type   = typename Analysis::value_type;
-  using pointer_type = typename Analysis::pointer_type;
 
-  FunctorType m_functor;
+  using Pointer   = typename Analysis::pointer_type;
+  using ValueType = typename Analysis::value_type;
+
+  Functor m_functor;
   Policy m_policy;
   ReducerType m_reducer;
-  pointer_type m_result_ptr;
+  Pointer m_result_ptr;
 
  public:
+  template <class ViewType>
+  ParallelReduce(const Functor& functor, const Policy& policy,
+                 const ViewType& result,
+                 std::enable_if_t<Kokkos::is_view_v<ViewType>>* = nullptr)
+      : m_functor(functor),
+        m_policy(policy),
+        m_reducer(InvalidType()),
+        m_result_ptr(result.data()) {}
+
+  ParallelReduce(const Functor& functor, const Policy& policy,
+                 const ReducerType& reducer)
+      : m_functor(functor),
+        m_policy(policy),
+        m_reducer(reducer),
+        m_result_ptr(reducer.view().data()) {}
+
   void execute() const {
     auto league_size   = m_policy.league_size();
     auto team_size     = m_policy.team_size();
     auto vector_length = m_policy.impl_vector_length();
 
-    value_type tmp;
+    ValueType val;
     typename Analysis::Reducer final_reducer(
         &ReducerConditional::select(m_functor, m_reducer));
-    final_reducer.init(&tmp);
+    final_reducer.init(&val);
 
     Kokkos::Experimental::Impl::OpenACCParallelReduceTeamHelper(
-        Kokkos::Experimental::Impl::FunctorAdapter<FunctorType, Policy>(
-            m_functor),
+        Kokkos::Experimental::Impl::FunctorAdapter<Functor, Policy>(m_functor),
         std::conditional_t<is_reducer_v<ReducerType>, ReducerType,
-                           Sum<value_type>>(tmp),
+                           Sum<ValueType>>(val),
         m_policy);
 
-    m_result_ptr[0] = tmp;
+    *m_result_ptr = val;
   }
-
-  template <class ViewType>
-  ParallelReduce(const FunctorType& arg_functor, const Policy& arg_policy,
-                 const ViewType& arg_result_view,
-                 std::enable_if_t<Kokkos::is_view_v<ViewType>>* = nullptr)
-      : m_functor(arg_functor),
-        m_policy(arg_policy),
-        m_reducer(InvalidType()),
-        m_result_ptr(arg_result_view.data()) {}
-
-  ParallelReduce(const FunctorType& arg_functor, const Policy& arg_policy,
-                 const ReducerType& reducer)
-      : m_functor(arg_functor),
-        m_policy(arg_policy),
-        m_reducer(reducer),
-        m_result_ptr(reducer.view().data()) {}
 };
 
 #ifdef KOKKOS_ENABLE_OPENACC_COLLAPSE_HIERARCHICAL_CONSTRUCTS
@@ -263,124 +258,6 @@ void OpenACCParallelReduceTeamBOr(Policy const policy, ValueType& aval,
 }
 
 }  // namespace Kokkos::Experimental::Impl
-
-namespace Kokkos {
-
-// Hierarchical Parallelism -> Team thread level implementation
-#pragma acc routine seq
-template <typename iType, class Lambda, typename ValueType>
-KOKKOS_INLINE_FUNCTION std::enable_if_t<!Kokkos::is_reducer_v<ValueType>>
-parallel_reduce(const Impl::TeamThreadRangeBoundariesStruct<
-                    iType, Impl::OpenACCTeamMember>& loop_boundaries,
-                const Lambda& lambda, ValueType& result) {
-  ValueType tmp = ValueType();
-  iType j_start =
-      loop_boundaries.team.team_rank() / loop_boundaries.team.vector_length();
-  if (j_start == 0) {
-#pragma acc loop seq
-    for (iType i = loop_boundaries.start; i < loop_boundaries.end; i++)
-      lambda(i, tmp);
-    result = tmp;
-  }
-}
-
-#pragma acc routine seq
-template <typename iType, class Lambda, typename ReducerType>
-KOKKOS_INLINE_FUNCTION std::enable_if_t<Kokkos::is_reducer_v<ReducerType>>
-parallel_reduce(const Impl::TeamThreadRangeBoundariesStruct<
-                    iType, Impl::OpenACCTeamMember>& loop_boundaries,
-                const Lambda& lambda, const ReducerType& reducer) {
-  using ValueType = typename ReducerType::value_type;
-  ValueType tmp;
-  reducer.init(tmp);
-  iType j_start =
-      loop_boundaries.team.team_rank() / loop_boundaries.team.vector_length();
-  if (j_start == 0) {
-#pragma acc loop seq
-    for (iType i = loop_boundaries.start; i < loop_boundaries.end; i++)
-      lambda(i, tmp);
-    reducer.reference() = tmp;
-  }
-}
-
-// FIXME_OPENACC: custom reduction is not implemented.
-template <typename iType, class Lambda, typename ValueType, class JoinType>
-KOKKOS_INLINE_FUNCTION void parallel_reduce(
-    const Impl::TeamThreadRangeBoundariesStruct<iType, Impl::OpenACCTeamMember>&
-        loop_boundaries,
-    const Lambda& lambda, const JoinType& join, ValueType& init_result) {
-  static_assert(!Kokkos::Impl::always_true<Lambda>::value,
-                "custom reduction is not implemented");
-}
-
-// Hierarchical Parallelism -> Thread vector level implementation
-#pragma acc routine seq
-template <typename iType, class Lambda, typename ValueType>
-KOKKOS_INLINE_FUNCTION std::enable_if_t<!Kokkos::is_reducer_v<ValueType>>
-parallel_reduce(const Impl::ThreadVectorRangeBoundariesStruct<
-                    iType, Impl::OpenACCTeamMember>& loop_boundaries,
-                const Lambda& lambda, ValueType& result) {
-  ValueType tmp = ValueType();
-  iType j_start =
-      loop_boundaries.team.team_rank() % loop_boundaries.team.vector_length();
-  if (j_start == 0) {
-#pragma acc loop seq
-    for (iType i = loop_boundaries.start; i < loop_boundaries.end; i++) {
-      lambda(i, tmp);
-    }
-    result = tmp;
-  }
-}
-
-#pragma acc routine seq
-template <typename iType, class Lambda, typename ReducerType>
-KOKKOS_INLINE_FUNCTION std::enable_if_t<Kokkos::is_reducer_v<ReducerType>>
-parallel_reduce(const Impl::ThreadVectorRangeBoundariesStruct<
-                    iType, Impl::OpenACCTeamMember>& loop_boundaries,
-                const Lambda& lambda, const ReducerType& reducer) {
-  using ValueType = typename ReducerType::value_type;
-  ValueType tmp;
-  reducer.init(tmp);
-  iType j_start =
-      loop_boundaries.team.team_rank() % loop_boundaries.team.vector_length();
-  if (j_start == 0) {
-#pragma acc loop seq
-    for (iType i = loop_boundaries.start; i < loop_boundaries.end; i++) {
-      lambda(i, tmp);
-    }
-    reducer.reference() = tmp;
-  }
-}
-
-// FIXME_OPENACC: custom reduction is not implemented.
-template <typename iType, class Lambda, typename ValueType, class JoinType>
-KOKKOS_INLINE_FUNCTION void parallel_reduce(
-    const Impl::ThreadVectorRangeBoundariesStruct<
-        iType, Impl::OpenACCTeamMember>& loop_boundaries,
-    const Lambda& lambda, const JoinType& join, ValueType& init_result) {
-  static_assert(!Kokkos::Impl::always_true<Lambda>::value,
-                "custom reduction is not implemented");
-}
-
-// Hierarchical Parallelism -> Team vector level implementation
-template <typename iType, class Lambda, typename ValueType>
-KOKKOS_INLINE_FUNCTION void parallel_reduce(
-    const Impl::TeamVectorRangeBoundariesStruct<iType, Impl::OpenACCTeamMember>&
-        loop_boundaries,
-    const Lambda& lambda, ValueType& result) {
-  ValueType tmp = ValueType();
-  iType j_start =
-      loop_boundaries.team.team_rank() % loop_boundaries.team.vector_length();
-  if (j_start == 0) {
-#pragma acc loop seq
-    for (iType i = loop_boundaries.start; i < loop_boundaries.end; i++) {
-      lambda(i, tmp);
-    }
-    result = tmp;
-  }
-}
-
-}  // namespace Kokkos
 
 #else /* #ifdef KOKKOS_ENABLE_OPENACC_COLLAPSE_HIERARCHICAL_CONSTRUCTS */
 
@@ -554,103 +431,6 @@ void OpenACCParallelReduceTeamBOr(Policy const policy, ValueType& aval,
 
 }  // namespace Kokkos::Experimental::Impl
 
-namespace Kokkos {
-
-// Hierarchical Parallelism -> Team thread level implementation
-#pragma acc routine worker
-template <typename iType, class Lambda, typename ValueType>
-KOKKOS_INLINE_FUNCTION std::enable_if_t<!Kokkos::is_reducer_v<ValueType>>
-parallel_reduce(const Impl::TeamThreadRangeBoundariesStruct<
-                    iType, Impl::OpenACCTeamMember>& loop_boundaries,
-                const Lambda& lambda, ValueType& result) {
-  ValueType tmp = ValueType();
-#pragma acc loop worker reduction(+ : tmp)
-  for (iType i = loop_boundaries.start; i < loop_boundaries.end; i++)
-    lambda(i, tmp);
-  result = tmp;
-}
-
-#pragma acc routine worker
-template <typename iType, class Lambda, typename ReducerType>
-KOKKOS_INLINE_FUNCTION std::enable_if_t<Kokkos::is_reducer_v<ReducerType>>
-parallel_reduce(const Impl::TeamThreadRangeBoundariesStruct<
-                    iType, Impl::OpenACCTeamMember>& loop_boundaries,
-                const Lambda& lambda, const ReducerType& reducer) {
-  using ValueType = typename ReducerType::value_type;
-  ValueType tmp = ValueType();
-  reducer.init(tmp);
-#pragma acc loop worker reduction(+ : tmp)
-  for (iType i = loop_boundaries.start; i < loop_boundaries.end; i++)
-    lambda(i, tmp);
-  reducer.reference() = tmp;
-}
-
-// FIXME_OPENACC: custom reduction is not implemented.
-template <typename iType, class Lambda, typename ValueType, class JoinType>
-KOKKOS_INLINE_FUNCTION void parallel_reduce(
-    const Impl::TeamThreadRangeBoundariesStruct<iType, Impl::OpenACCTeamMember>&
-        loop_boundaries,
-    const Lambda& lambda, const JoinType& join, ValueType& init_result) {
-  static_assert(!Kokkos::Impl::always_true<Lambda>::value,
-                "custom reduction is not implemented");
-}
-
-// Hierarchical Parallelism -> Thread vector level implementation
-#pragma acc routine vector
-template <typename iType, class Lambda, typename ValueType>
-KOKKOS_INLINE_FUNCTION void parallel_reduce(
-    const Impl::ThreadVectorRangeBoundariesStruct<
-        iType, Impl::OpenACCTeamMember>& loop_boundaries,
-    const Lambda& lambda, ValueType& result) {
-  ValueType tmp = ValueType();
-#pragma acc loop vector reduction(+ : tmp)
-  for (iType i = loop_boundaries.start; i < loop_boundaries.end; i++) {
-    lambda(i, tmp);
-  }
-  result = tmp;
-}
-
-#pragma acc routine vector
-template <typename iType, class Lambda, typename ReducerType>
-KOKKOS_INLINE_FUNCTION std::enable_if_t<Kokkos::is_reducer_v<ReducerType>>
-parallel_reduce(const Impl::ThreadVectorRangeBoundariesStruct<
-                    iType, Impl::OpenACCTeamMember>& loop_boundaries,
-                const Lambda& lambda, const ReducerType & reducer) {
-  using ValueType = typename ReducerType::value_type;
-  ValueType tmp;
-  reducer.init(tmp);
-#pragma acc loop vector reduction(+ : tmp)
-  for (iType i = loop_boundaries.start; i < loop_boundaries.end; i++) {
-    lambda(i, tmp);
-  }
-  reducer.reference() = tmp;
-}
-
-// FIXME_OPENACC: custom reduction is not implemented.
-template <typename iType, class Lambda, typename ValueType, class JoinType>
-KOKKOS_INLINE_FUNCTION void parallel_reduce(
-    const Impl::ThreadVectorRangeBoundariesStruct<
-        iType, Impl::OpenACCTeamMember>& loop_boundaries,
-    const Lambda& lambda, const JoinType& join, ValueType& init_result) {
-  static_assert(!Kokkos::Impl::always_true<Lambda>::value,
-                "custom reduction is not implemented");
-}
-
-// Hierarchical Parallelism -> Team vector level implementation
-#pragma acc routine vector
-template <typename iType, class Lambda, typename ValueType>
-KOKKOS_INLINE_FUNCTION void parallel_reduce(
-    const Impl::TeamVectorRangeBoundariesStruct<iType, Impl::OpenACCTeamMember>&
-        loop_boundaries,
-    const Lambda& lambda, ValueType& result) {
-  ValueType tmp = ValueType();
-#pragma acc loop vector reduction(+ : tmp)
-  for (iType i = loop_boundaries.start; i < loop_boundaries.end; i++) {
-    lambda(i, tmp);
-  }
-  result = tmp;
-}
-}  // namespace Kokkos
 #endif /* #if 0 */
 
 #endif /* #ifdef KOKKOS_ENABLE_OPENACC_COLLAPSE_HIERARCHICAL_CONSTRUCTS */
@@ -697,4 +477,4 @@ KOKKOS_IMPL_OPENACC_PARALLEL_REDUCE_TEAM_HELPER(BOr);
 
 #undef KOKKOS_IMPL_OPENACC_PARALLEL_REDUCE_TEAM_HELPER
 
-#endif /* #ifndef KOKKOS_OPENACC_PARALLEL_REDUCE_TEAM_HPP */
+#endif

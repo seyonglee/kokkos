4db80157cbae67e33031eeb358a8e68aced4cb5e
Damien L-G <dalg24@gmail.com>
2025-09-20 17:03:57 -0600

    [OpenMPTarget] Get rid of outdated macro guards
    We only support recent LLVM/Clang versions
    
    Signed-off-by: Damien L-G <dalg24@gmail.com>

--- core/unit_test/TestReducers.hpp
+++ core/unit_test/TestReducers.hpp
@@ -1584,16 +1584,8 @@ struct TestReducers {
 #endif
 // FIXME_OPENACC - OpenACC (V3.3) does not support custom reductions.
 #if !defined(KOKKOS_ENABLE_OPENACC)
-// FIXME_OPENMPTARGET - The minmaxloc test fails llvm < 13 version,
-// test_minmaxloc_2d requires custom reductions
-#if defined(KOKKOS_ENABLE_OPENMPTARGET)
-#if defined(KOKKOS_COMPILER_CLANG) && (KOKKOS_COMPILER_CLANG >= 1300) && \
-    (KOKKOS_COMPILER_CLANG <= 1700)
+#if defined(KOKKOS_ENABLE_OPENMPTARGET)  // FIXME_OPENMPTARGET custom reducers
     test_minmaxloc(10007);
-#else
-    if (!std::is_same_v<ExecSpace, Kokkos::Experimental::OpenMPTarget>)
-      test_minmaxloc(10007);
-#endif
 #else
     test_minmaxloc(10007);
     test_minmaxloc_loc_init(3);
@@ -1640,15 +1632,8 @@ struct TestReducers {
 #endif
 // FIXME_OPENACC - OpenACC (V3.3) does not support custom reductions.
 #if !defined(KOKKOS_ENABLE_OPENACC)
-// FIXME_OPENMPTARGET - The minmaxloc test fails llvm < 13 version,
-// the minmaxloc_2d test requires custom reductions.
-#if defined(KOKKOS_ENABLE_OPENMPTARGET)
-#if defined(KOKKOS_COMPILER_CLANG) && (KOKKOS_COMPILER_CLANG >= 1300)
+#if defined(KOKKOS_ENABLE_OPENMPTARGET)  // FIXME_OPENMPTARGET custom reducers
     test_minmaxloc(10007);
-#else
-    if (!std::is_same_v<ExecSpace, Kokkos::Experimental::OpenMPTarget>)
-      test_minmaxloc(10007);
-#endif
 #else
     test_minmaxloc(10007);
     test_minmaxloc_loc_init(3);

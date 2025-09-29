d1d6b2d5de09a66b6b92a3c017bc903f10dda671
JBludau <104908666+JBludau@users.noreply.github.com>
2025-09-29 17:21:36 -0400

    Desul atomics: Add 128bit CAS for >CC90 on NVIDIA (#8025)
    * use 128bit cas for >CC90
    * have the hpsf ci do the testing
    * sync with desul
    * use more readable guards for cuda atomic selection
    
    ---------
    
    Signed-off-by: Jakob Bludau <bludauj@ornl.gov>
    Co-authored-by: Damien L-G <dalg24+github@gmail.com>

--- core/src/impl/Kokkos_DesulAtomicsConfig.hpp
+++ core/src/impl/Kokkos_DesulAtomicsConfig.hpp
@@ -17,13 +17,22 @@
 #ifndef KOKKOS_DESUL_ATOMICS_CONFIG_HPP
 #define KOKKOS_DESUL_ATOMICS_CONFIG_HPP
 
-#if defined(KOKKOS_ARCH_KEPLER) || defined(KOKKOS_ARCH_MAXWELL)
+#include <impl/Kokkos_NvidiaGpuArchitectures.hpp>
+
+#ifdef KOKKOS_IMPL_ARCH_NVIDIA_GPU
+
+#if KOKKOS_IMPL_ARCH_NVIDIA_GPU < 60
 #define DESUL_CUDA_ARCH_IS_PRE_PASCAL
 #endif
 
-#if defined(KOKKOS_ARCH_KEPLER) || defined(KOKKOS_ARCH_MAXWELL) || \
-    defined(KOKKOS_ARCH_PASCAL)
+#if KOKKOS_IMPL_ARCH_NVIDIA_GPU < 70
 #define DESUL_CUDA_ARCH_IS_PRE_VOLTA
+#endif
+
+#if KOKKOS_IMPL_ARCH_NVIDIA_GPU < 90
+#define DESUL_CUDA_ARCH_IS_PRE_HOPPER
+#endif
+
 #endif
 
 #endif

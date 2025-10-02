0deaecd0ee930ad671efa32951607f1389e719d5
Seyong Lee <lees2@ornl.gov>
2025-10-02 13:41:01 -0400

    Update core/src/OpenACC/Kokkos_OpenACC_Team.hpp
    Co-authored-by: Damien L-G <dalg24+github@gmail.com>
    Signed-off-by: Seyong Lee <lees2@ornl.gov>

--- core/src/OpenACC/Kokkos_OpenACC_Team.hpp
+++ core/src/OpenACC/Kokkos_OpenACC_Team.hpp
@@ -301,7 +301,7 @@ class TeamPolicyInternal<Kokkos::Experimental::OpenACC, Properties...>
 
   TeamPolicyInternal(int league_size_request, int team_size_request,
                      int vector_length_request = 1)
-      : m_space(typename traits::execution_space()),
+      : m_space(),
         m_team_scratch_size{0, 0},
         m_thread_scratch_size{0, 0},
         m_tune_team_size(false),
